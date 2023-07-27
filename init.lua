568d27e7ce652d4beec5d9b0fd050ac1b9c3b2bd
Mechanel <nyutanislavsky@qq.com>
2023-07-28 01:22:20 +0800

    fix zhangfei

--- init.lua
+++ init.lua
@@ -1286,7 +1286,7 @@ local ex__paoxiao_trigger = fk.CreateTriggerSkill{
   frequency = Skill.Compulsory,
   events = {fk.DamageCaused},
   can_trigger = function(self, event, target, player, data)
-    return target == player and player:hasSkill(self.name) and data.card.trueName == "slash" and player:getMark("@paoxiao-turn") ~= 0 and not data.chain and player.phase == Player.Play
+    return target == player and player:hasSkill(self.name) and data.card and data.card.trueName == "slash" and player:getMark("@paoxiao-turn") ~= 0 and not data.chain and player.phase == Player.Play
   end,
   on_use = function(self, event, target, player, data)
     data.damage = data.damage + player:getMark("@paoxiao-turn")
@@ -1337,7 +1337,7 @@ zhangfei:addSkill(ex__tishen)
 Fk:loadTranslationTable{
   ["ex__zhangfei"] = "界张飞",
   ["ex__paoxiao"] = "咆哮",
-  [":ex__paoxiao"] = "①，锁定技，你使用杀无次数限制;②，锁定技，你的回合内，你使用杀被闪避时，你获得一枚【咆】;③，当你于回合内使用杀造成伤害时，若你有【咆】，你弃置所有【咆】令此杀伤害+X(X为你弃置的【咆】数量);④，回合结束后，你失去所有【咆】。",
+  [":ex__paoxiao"] = "锁定技①你使用杀无次数限制；②你的回合内，你使用【杀】被闪避时，你获得一枚“咆”；③当你于回合内使用【杀】造成伤害时，若你有“咆”，你弃置所有“咆”令此【杀】伤害+X(X为你弃置的“咆”数量)；④回合结束后，你失去所有“咆”。",
   ["ex__tishen"] = "替身",
   [":ex__tishen"] = "限定技，准备阶段，若你已受伤，则你可以将体力值回复至上限，然后摸回复数值张牌。",
   ["@paoxiao-turn"] = "咆",
@@ -1349,7 +1349,6 @@ Fk:loadTranslationTable{
   ["$ex__tishen1"] = "欺我无谋，定要尔等血偿！",
   ["$ex__tishen2"] = "谁?还敢过来一战！",
   ["~ex__zhangfei"] = "桃园一拜，此生…无憾……",
-
 }
 local ex__jizhi = fk.CreateTriggerSkill{
   name = "ex__jizhi",
